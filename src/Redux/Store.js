3ce0c9398833fa796adfdb160c5bf198f301da54
hiro1125 <hiro112538@gmail.com>
2022-06-28 21:54:56 +0900

    CounterReducer settings

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./CounterSlice";
 
 export const Store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
